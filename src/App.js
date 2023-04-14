f511311b6068a97099c59df3745adb5b12e3c8c1
JacquesGodin <tiagobgodinho@gmail.com>
2023-04-14 01:42:00 +0100

    Add vote chart functionality

--- src/App.js
+++ src/App.js
@@ -5,31 +5,58 @@ import logo from './cover.png';
 import ContractButtons from './ContractButtons';
 import contractABI from './contractABI.json';
 import "./App.css";
-
+import VoteChart from './VoteChart';
 
 function App() {
   const [haveMetamask, sethaveMetamask] = useState(true);
   const [accountAddress, setAccountAddress] = useState('');
   const [accountBalance, setAccountBalance] = useState('');
   const [isConnected, setIsConnected] = useState(false);
-  const CONTRACT_ADDRESS = '0x30FbAccE7b346748C9a0Da3b93A59A00ED67cE7f';
+  const CONTRACT_ADDRESS = '0xbAEa03DBAe6dDB850A30EE8e060a23e637106170';
+
+// Initialize chart data
+  const [chartData, setChartData] = useState(
+   Array.from({ length: 8 }, (_, index) => ({
+      optionIndex: index,
+      totalTokens: 0,
+    }))
+  );
 
 
   const { ethereum } = window;
   const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
+  
+
+  const setupVoteCastEventListener = async () => {
+    if (!provider || !contract) return;
+  
+    contract.on('VoteCast', (optionIndex, totalTokens, event) => {
+      setChartData((prevData) => {
+        const newData = [...prevData];
+        const index = newData.findIndex(
+          (item) => item.optionIndex === optionIndex.toNumber()
+        );
+        if (index !== -1) {
+          newData[index].totalTokens = totalTokens.toNumber();
+        }
+        return newData;
+      });
+    });
+  };
+  
+
 
   const handleButtonClick = async (index) => {
-    console.log(`Button ${index + 1} clicked`);
+    console.log(`Button ${index} clicked`);
   
     if (!window.ethereum) {
       alert('Please install MetaMask to use this dApp!');
       return;
     }
   
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
-  
+    
     const tokenAmount = parseInt(prompt('Enter the number of tokens you want to use for voting (0-50):'), 10);
     if (isNaN(tokenAmount) || tokenAmount < 0 || tokenAmount > 50) {
       alert('Invalid token amount. Please enter a number between 0 and 50.');
@@ -45,10 +72,12 @@ function App() {
       alert('Failed to submit the vote');
     }
   };
+
+  useEffect(() => {
+    setupVoteCastEventListener();
+  }, [provider, contract]);
   
   
-
-
   useEffect(() => {
     const { ethereum } = window;
     const checkMetamaskAvailability = async () => {
@@ -60,6 +89,28 @@ function App() {
     checkMetamaskAvailability();
   }, []);
 
+  const fetchVoteCounts = async () => {
+    console.log('Fetching vote counts...');
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
+  
+    const updatedChartData = [...chartData];
+  
+    try {
+      for (let i = 0; i < 8; i++) {
+        const [_, voteCount] = await contract.getOption(i);
+        console.log(`Vote count for index ${i}:`, (await voteCount).toString());
+        updatedChartData[i] = { ...updatedChartData[i], totalTokens: (await voteCount).toNumber() };
+      }
+      setChartData(updatedChartData);
+    } catch (err) {
+      console.error(err);
+      alert('Error fetching vote counts');
+    }
+  };
+  
+  
+  
   const connectWallet = async () => {
     if (!window.ethereum) {
       alert('Please install MetaMask to use this dApp!');
@@ -117,6 +168,10 @@ function App() {
               <>
                 <p className="info">🎉 Connected Successfully</p>
                 <ContractButtons onButtonClick={handleButtonClick} />
+                <VoteChart data={chartData} />
+                <button className="btn" onClick={fetchVoteCounts}>
+                  Fetch Vote Counts
+                </button>
               </>
             ) : (
               <button className="btn" onClick={connectWallet}>
@@ -130,6 +185,6 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+        }  
+  export default App;
+  
